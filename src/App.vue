eb0ec8c762637e1e2877f22cb8593715ae036084
jmll <1692567805@qq.com>
2021-03-06 00:52:34 +0800

    连接高德地图 API,实现基础地图的展示

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!--    <img src="./assets/logo.png">-->
+    <div id="container" class="container"> </div>
+    <!--    <HelloWorld/>-->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    },
+    mounted() {
+      this.initMap();
+    },
+    methods: {
+      initMap() {
+        this.map = new AMap.Map('container', {
+          zoom:11,//级别
+          center: [118.92137, 32.10467]//中心点坐标
+        });
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .container {
+  / / 容器大小
+    /*margin-left: %;*/
+  width: 100%;
+    height: 850px;
+  }
 </style>
