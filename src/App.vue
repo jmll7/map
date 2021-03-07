bc5b5308b5b4d2457f0868fea9fd3f2168f099ea
jmll <1692567805@qq.com>
2021-03-08 00:31:58 +0800

    改变地图在容器中的分布
    Signed-off-by: jmll <1692567805@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <!--    <img src="./assets/logo.png">-->
-    <div id="container" class="container"> </div>
-    <!--    <HelloWorld/>-->
+    <!-- <img src="./assets/logo.png"> -->
+    <div id="container" class="container"></div>
+    <!-- <HelloWorld/> -->
   </div>
 </template>
 
@@ -20,8 +20,10 @@
     methods: {
       initMap() {
         this.map = new AMap.Map('container', {
-          zoom:11,//级别
-          center: [118.92137, 32.10467]//中心点坐标
+          zoom:11,
+          // 级别
+          center: [118.92137, 32.10467]
+          // 中心点坐标
         });
       }
     }
@@ -29,18 +31,18 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+  /*#app {*/
+  /*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*  -webkit-font-smoothing: antialiased;*/
+  /*  -moz-osx-font-smoothing: grayscale;*/
+  /*  text-align: center;*/
+  /*  color: #2c3e50;*/
+  /*  margin-top: 60px;*/
+  /*}*/
   .container {
-  / / 容器大小
+    /*容器大小*/
     /*margin-left: %;*/
-  width: 100%;
-    height: 850px;
+    width: 100%;
+    height: 98vh;
   }
 </style>
